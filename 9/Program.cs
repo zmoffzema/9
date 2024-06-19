ebee7535b8856737791a0f1013e19e2949d68da0
z_moff <zmoffzema@gmail.com>
2024-06-20 00:00:17 +0300

    Добавьте файлы проекта.

---
+++ 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = "Hello, world!";
+        string reversedString = ReverseString(input);
+        Console.WriteLine("Reversed string: " + reversedString);
+    }
+
+    public static string ReverseString(string str)
+    {
+        if (str == null)
+        {
+            throw new ArgumentException("Input string cannot be null");
+        }
+
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
